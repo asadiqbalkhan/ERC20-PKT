f2dd1137a58af692ff467344e667f06bdd05dda9
asadiqbalkhan <iq.asad@gmail.com>
2021-07-31 09:42:56 +0500

    update UI later

--- pkt-token/src/App.js
+++ pkt-token/src/App.js
@@ -7,14 +7,21 @@ import PAKToken from "./abi/PAKToken.json";
 import UniSwap from "./UniSwap";
 
 const infuraProjectId = "ff5964fc8ce94dc0b44cd1c5d071ce21";
+const Contract = require("web3-eth-contract");
 
 function App() {
   const web3Context = useWeb3(
     `https://ropsten.infura.io/v3/${infuraProjectId}`
   );
+  // 0xa5aFEe78e75EBaffdEE9Dcc2829bCf4cACA44463
+  const { networkId, networkName, account, providerName } = web3Context;
 
-  const { networkId, networkName, account, providerName, contract } =
-    web3Context;
+  const contract = new Contract(
+    PAKToken.abi,
+    "0x82C83FdfAa75a3400C8Fc321d1BCc243358f8Ed5"
+  );
+
+  console.log(contract);
 
   return (
     <div className="App">
