79bdcd41a0decebf4faa9bb5bcbb6e405b08f4f0
asadiqbalkhan <iq.asad@gmail.com>
2021-07-31 07:40:20 +0500

    setup web3 with networkjs

--- pkt-token/src/App.js
+++ pkt-token/src/App.js
@@ -1,9 +1,20 @@
 import "./App.css";
+import Web3 from "web3";
+import Web3Info from "./Web3Info.js";
+import { useWeb3 } from "@openzeppelin/network/react";
+
+const infuraProjectId = "ff5964fc8ce94dc0b44cd1c5d071ce21";
 
 function App() {
+  const web3Context = useWeb3(
+    `https://ropsten.infura.io/v3/${infuraProjectId}`
+  );
+  const { networkId, networkName, providerName } = web3Context;
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Pak Digital Rupee - PKT ERC20</h1>
+      <Web3Info title="Web3 Info" web3Context={web3Context} />
     </div>
   );
 }
---
+++ pkt-token/src/Web3Info.js
@@ -0,0 +1,39 @@
+import React, { useCallback } from "react";
+
+const Web3Info = (props) => {
+  const { web3Context } = props;
+  const { networkId, networkName, accounts, providerName } = web3Context;
+
+  const requestAuth = async (web3Context) => {
+    try {
+      await web3Context.requestAuth();
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const requestAccess = useCallback(() => requestAuth(web3Context, []));
+  return (
+    <div>
+      <h3> {props.title} </h3>
+      <div>
+        Network: {networkId ? `${networkId} – ${networkName}` : "No connection"}
+      </div>
+      <div>
+        Your address: {accounts && accounts.length ? accounts[0] : "Unknown"}
+      </div>
+      <div>Provider: {providerName}</div>
+      {accounts && accounts.length ? (
+        <div>Accounts & Signing Status: Access Granted</div>
+      ) : !!networkId && providerName !== "infura" ? (
+        <div>
+          <button onClick={requestAccess}>Request Access</button>
+        </div>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  );
+};
+
+export default Web3Info;
